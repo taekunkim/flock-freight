b8d125fd8b9ba56e8a99c9e3ce93d491201ef874
Tae Kun Kim <taekunkim214@gmail.com>
2023-03-04 18:36:56 -0800

    adds new data cleaning functions

--- src/features/data_cleaning.py
+++ src/features/data_cleaning.py
@@ -112,12 +112,14 @@ def get_remaining_time(df, past="CREATED_ON_HQ", future="PICKUP_DEADLINE_PST", n
     # deep copy
     df = df.copy()
 
-    df[new_col] = (df[future] - df[past]).dt.total_seconds()
+    df[new_col] = (df[future] - df[past]).dt.total_seconds() // 3600
+
+    # ensure that there are not negative times
     df = df[df[new_col]>=0].reset_index(drop=True)
 
     return df
 
-def during_business_hours(df):
+def during_business_hours(df, col, new_col):
     """
     Adds a column that shows whether an offer was made during business hours.
 
@@ -128,25 +130,26 @@ def during_business_hours(df):
     # deep copy
     df = df.copy()
 
-    hours = df["CREATED_ON_HQ"].dt.hour
+    hours = df[col].dt.hour
     hours = ((hours >= 8) | (hours <= 18))
 
-    days = df["CREATED_ON_HQ"].dt.dayofweek
+    days = df[col].dt.dayofweek
     days = days < 5
 
     during_business_hours = hours & days
 
-    df["BUSINESS_HOURS"] = during_business_hours
+    df[new_col] = during_business_hours
 
     return df
 
-def get_prorated_rate(df, delivered_only=False):
+def get_prorated_rate(df, delivered_only=False, overwrite=True):
     """
     Calculates prorated cost of pooled offers.
 
     Args:
         df (DataFrame): DataFrame to perform calculations on
         delivered_only (boolean): whether to filter for only offers that were chosen
+        over_write(boolean): whether to overwrite RATE_USD with the prorated rate
 
     """
 
@@ -163,8 +166,11 @@ def get_prorated_rate(df, delivered_only=False):
     # records the sum of [linear feet] of all orders inlcuded in an offer
     total_feet = df.groupby("OFFER_ID")["PALLETIZED_LINEAR_FEET"].transform("sum")
 
+    # decides whether to overwrite
+    new_col = "RATE_USD" if overwrite else "PRORATED_RATE_USD"
+
     # calculates the prorated rate
-    df["PRORATED_RATE_USD"] = df["RATE_USD"] * df["PALLETIZED_LINEAR_FEET"] / total_feet
+    df[new_col] = (df["RATE_USD"] * df["PALLETIZED_LINEAR_FEET"] / total_feet) * (df["OFFER_TYPE"] == "pool") + df["RATE_USD"] * (df["OFFER_TYPE"] == "quote")
 
     # drops OFFER_ID
     df = df.drop("OFFER_ID", axis=1)
@@ -207,13 +213,32 @@ def get_business_hours(df, order="ORDER_DATETIME_PST", pickup="PICKUP_DEADLINE_P
     
     def business_hrs(start, end):
         diff_hours = business_hours.difference(start, end)
-        return diff_hours.hours + float(diff_hours.seconds) / float(3600)
+        return diff_hours.hours + float(diff_hours.seconds) // float(3600)
     
     # Add a new column called BUSINESS_HOURS_ORDER_PICKUP
     df["BUSINESS_HOURS_ORDER_PICKUP"] = df.apply(
         lambda row: business_hrs(row[order], 
                                  row[pickup]), axis=1
     )
+    return df
+
+def is_weekend(df, col, new_col):
+    """checks if a date in col falls on a weekend or holiday
+
+    Args:
+        df (_type_): dataframe to check 
+        col (_type_): name of the column to check
+        new_col (_type_): name of the new column
+    """
+
+    import holidays
+
+    # deep copy
+    df = df.copy()
+
+    CA_holidays = holidays.US(subdiv="CA")
+    df[new_col] = df[col].apply(lambda date: date not in CA_holidays)
+    
     return df
 
 def impute_mileage(df, drop=True):
@@ -278,7 +303,7 @@ def popular_cities(df, cols, threshold=0.005):
     return df
 
 def parse_datetime(df, cols=["ORDER_DATETIME_PST", "PICKUP_DEADLINE_PST"]):
-    
+
     # deep copy
     df = df.copy()
 
@@ -287,5 +312,96 @@ def parse_datetime(df, cols=["ORDER_DATETIME_PST", "PICKUP_DEADLINE_PST"]):
         df[col.split("_")[0]+"_DAY"] = datetime_col.dt.dayofweek
         df[col.split("_")[0]+"_MONTH"] = datetime_col.dt.month
         df[col.split("_")[0]+"_HOUR"] = datetime_col.dt.hour
+
+    return df
+
+def assign_ids(df, col):
+    """ maps original values in col to new values ranging from 1 to the length of unique values in col
+
+    Args:
+        df (dataframe): dataframe to update
+        col (str): name of the column to update
+    """
+
+    # deep copy
+    df = df.copy()
+
+    original_vals = df[col].unique()
+    original_to_new = dict(zip(original_vals, range(1, len(original_vals)+1)))
+    df[col] = df[col].apply(lambda original_val: original_to_new[original_val])
+
+    return df
+
+def change_to_boolean(df, col, new_col, true_val):
+    """changes binary categorical string data to boolean
+
+    Args:
+        df (dataframe): dataframe to update
+        col (str): name of the column to map
+        new_col (str): name of the new column
+        true_val (str): the value to be mapped to True
+    """
+
+    # deep copy
+    df = df.copy()
+
+    df[new_col] = df[col] == true_val
+    df = df.drop(col, axis=1)
+
+    return df
+
+def get_coordinates(df, col, new_col, dir="../data/zip3/zip3.shp"):
+    """appends a column of coordinates to a dataframe based on the 3-digit zipcode value in "col"
+
+    Args:
+        df (dataframe): dataframe to update
+        col (str): name of the 3-digit zipcode column
+        new_col (str): name of new column
+        dir (str): directory of zipcode shapefile
+    """
+
+    import geopandas as gpd
+
+    df = df.copy()
+
+    # read 3 zipcode map data
+    usa = gpd.read_file(dir)
+
+    # set map type
+    usa = usa.to_crs("EPSG:3395")
+
+    # add centroid coordinates
+    usa["x"], usa["y"] = usa.centroid.x, usa.centroid.y
+
+    # only show continental USA
+    usa = usa.query("STATE not in [\"HI\", \"AK\", \"PR\"]")
+    usa = usa[usa["x"]>-1.4*10**7]
+    usa = usa[usa["y"]>2.5*10**6]
+
+    # drop irrelevant columns
+    usa = usa[["ZIP3", "x", "y"]]
+
+    # merge dataframes
+    df = df.merge(usa, left_on=col, right_on="ZIP3").drop("ZIP3", axis=1).rename({"x": new_col+"_X", "y": new_col+"_Y"}, axis=1)
+
+    return df
+
+def extract_from_date(df, col, new_col, kind=["month", "day", "day of week", "week"]):
+    """
+    extracts temporal features from datetime col and adds new columns
+
+    Args:
+        df (dataframe): _description_
+        col (str): name of the datetime column
+        new_col (str): name of the new colum
+        kind (list, optional): types of features to extract. Defaults to ["month", "day", "day of week", "week"].
+    """
+
+    df = df.copy()
+
+    if "month" in kind: df[new_col+"_MONTH"] = df[col].dt.month
+    if "day" in kind: df[new_col+"_DAY"] = df[col].dt.day
+    if "day of week" in kind: df[new_col+"_DAY_OF_WEEK"] = df["CREATED_ON_HQ"].dt.dayofweek
+    if "week" in kind: df[new_col+"_WEEK"] = df[col].dt.isocalendar().week
 
     return df
